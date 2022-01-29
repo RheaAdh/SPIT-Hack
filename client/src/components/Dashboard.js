3fc34ba154e9ee098dc445af445ff26b86d92e06
RheaAdh <rheadhikari@gmail.com>
2022-01-29 11:58:25 +0530

    todo done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Todo from "./Todo";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
 };
 
 export default Dashboard;
--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -4,6 +4,7 @@ import { Redirect, Route, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 import { Navigate } from "react-router-dom";
+
 export default function PrivateRoute({ children }) {
   const auth = useAuth();
   return auth.user ? children : <Navigate to="/" />;
